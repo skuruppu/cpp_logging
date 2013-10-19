49a6c3e7baf399b037862874958e34f9d999b6c0
Shanika Kuruppu <shanika.kuruppu@gmail.com>
2013-10-19 17:41:20 +1100

    Printing out log level.

--- logging.h
+++ logging.h
@@ -27,6 +27,8 @@
 #define INCLUDE_LOG_H
 
 #include <iostream>
+#include <stdio.h>
+#include <string>
 
 #define DEBUG 0
 #define INFO 1
@@ -46,11 +48,41 @@ public:
     }
 };
 
+std::string log_level_to_str(const int log_level) {
+    char buf[12];
+    std::string str = "";
+
+    switch(log_level) {
+        case DEBUG:
+            str = "[DEBUG]";
+            break;
+        case INFO:
+            str = "[INFO]";
+            break;
+        case WARNING:
+            str = "[WARNING]";
+            break;
+        case ERROR:
+            str = "[ERROR]";
+            break;
+        case CRITICAL:
+            str = "[CRITICAL]";
+            break;
+        default:
+            break;
+    }
+
+    sprintf(buf, "%-11s ", str.c_str());
+    return buf;
+}
+
 // If the minimum log level requirement is satisfied then prints the log
 // message. This works because the & operator has lower precedence than the <<
 // operator, therefore the stream output is evaluated before the Log class is
 // initialised.
 #define LOG(log_level) log_level >= LOGLEVEL && \
-                       Log() & std::cerr << __FILE__ << ':' << __LINE__ << ':'
+                            Log() & std::cerr << \
+                            log_level_to_str(log_level) <<  \
+                            __FILE__ << ':' << __LINE__ << ": "
 
 #endif
